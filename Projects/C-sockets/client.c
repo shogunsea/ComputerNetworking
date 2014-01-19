240f124e481e0ff8a691e66f64482964f8277817
shogunsea <xinxiaokang@gmail.com>
2014-01-19 16:46:02 -0500

    comment and layout.

--- Projects/C-sockets/client.c
+++ Projects/C-sockets/client.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-//#include <stdlib.h>//  use strtol instead of atoi
-//#include <errno.h>  //don't need this one.
 #include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -8,28 +6,20 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
-// libs for ssl connection
-#include <openssl/ssl.h>
+#include <openssl/ssl.h> // libs for ssl connection
 //#include <openssl/err.h>
 //#include <openssl/rand.h>
+//#include <stdlib.h>//  use strtol instead of atoi
 
 #define DEFAULT_SSL_PORT 27994
 #define DEFAULT_PORT 27993
 #define BUFFER_LENGTH 256//'The maximum length of each message is 256 bytes'
 #define MESSAGE_LENGTH 256
-//#define "cs5700spring2014" "cs5700spring2014"
 #define INITIAL_MESSAGE "cs5700spring2014 HELLO "
 #define SOLUTION_LENGTH 100
 #define FLAG_LENGTH 64 
 
-
-void errorHandler(const char *msg)
-{
-    printf("%s\n",msg);
-    exit(0);
-}
-
-// rewrite this function.
+// function to parse mathematical string and evaluate the expression.
 int calculator(char *buffer)
 {
     //printf("buffer is: %s\n", buffer );
@@ -43,72 +33,69 @@ int calculator(char *buffer)
     int i=0;
     int j =0;
     
-    int sign=0;
+    int operator=0;
     // cs5700spring2014 STATUS 991 - 574
     // STATUS 991 - 574
     for (i=5;i<(int)strlen(equation);i++)
     {
-        
         if(equation[i]=='S'&&equation[i+1]==' ')
         {
             int k=0;
             memset(firstNumber, '\0', 5);
-            
             for(j=i+2;equation[j]!=' ';j++)
             {
                 firstNumber[k++]=equation[j];
             }
         }
-        
         // ascii 
         if(equation[i]>41&&equation[i]<48)
         {
-
-            sign = equation[i];
+            operator = equation[i];
             int k=0;
             memset(secondNumber,'\0',5);
-            
             for(j=i+2;equation[j]!='\n';j++ )
             {
                 secondNumber[k++]=equation[j];
             }
         }
     }
-    
-    
     int first = strtol(firstNumber,NULL,10);
     int second = strtol(secondNumber,NULL,10);
     int result = 0;
-    
-    switch(sign)
+    switch(operator)
     {
-        case(43):result = first + second;break;
-        case(45):result = first - second;break;
-        case(42):result = first * second;break;
-        case(47):result = first / second;break;
+        case(43):
+            result = first + second;
+            break;
+        case(45):
+            result = first - second;
+            break;
+        case(42):
+            result = first * second;
+            break;
+        case(47):
+            result = first / second;
+            break;
     }
-
-    
     return result;
 }
 
+void errorHandler(const char *msg)
+{
+    printf("%s\n",msg);
+    exit(0);
+}
 
 void normalConnection(int sfd, char *info)
 {   
 //    printf("sfd is:%i\n",sfd);
 //    printf("info is:%s\n",info);
-
     char buffer[BUFFER_LENGTH]={};
     char flagBuffer[BUFFER_LENGTH]={};
-
-    
     int result=0;
-
     char solution [SOLUTION_LENGTH] ={};
-
     
     write(sfd,info,strlen(info));
-    
     read (sfd, buffer,BUFFER_LENGTH);
     
     int a = 0;
@@ -117,38 +104,23 @@ void normalConnection(int sfd, char *info)
         a++;
        //printf("calculating!\n");
        //printf("buffer is: %s\n",buffer);
-        
         result = calculator(buffer);
-
       //printf("result is: %i\n", result);
       //printf("solution before sprintf: %s\n", solution);
-        
        sprintf(solution, "%s %d\n","cs5700spring2014", result);
       //printf("solution after sprintf: %s\n", solution);
-
-        
         memset(buffer, '\0', BUFFER_LENGTH);
-        
         write(sfd,solution,strlen(solution));
-        
         read (sfd, buffer,BUFFER_LENGTH);
     }
-    
-    printf("Normal Connection:\n");
-    printf("FLAG_LENGTH: %i\n",FLAG_LENGTH);
-    printf("buffer: %s\n", buffer);
-    printf("flagBuffer: %s\n", flagBuffer);
-
-
+    // printf("Normal Connection:\n");
+    // printf("FLAG_LENGTH: %i\n",FLAG_LENGTH);
+    // printf("buffer: %s\n", buffer);
+    // printf("flagBuffer: %s\n", flagBuffer);
     memcpy(flagBuffer, buffer+sizeof("cs5700spring2014"), FLAG_LENGTH);
-    
     printf("Your secret flag is: \n%s\n",flagBuffer);
     //printf("total %i equations.\n",a);
-
 }
-
-
-
 //http://www.openssl.org/docs/ssl/ssl.html   # openssl lib documentation
 //http://savetheions.com/2010/01/16/quickly-using-openssl-in-c/   # tutorial for how to use openssl in c
 void SSLConnection(int sfd, char *info)
@@ -156,36 +128,27 @@ void SSLConnection(int sfd, char *info)
     //printf("Inside ssl function, sockfd: %i\n",sfd);
     SSL *sslHandle;
     SSL_CTX *sslContext;
-    
-
      // Register the error strings for libcrypto & libssl
     SSL_load_error_strings();
      // Register the available ciphers and digests
     SSL_library_init();
-    
     // New context saying we are a client, and using SSL 2 or 3
     sslContext = SSL_CTX_new (SSLv23_client_method());
     if (sslContext == NULL)
         errorHandler("ERROR SSL context");
-    
      // Create an SSL struct for the connection
     sslHandle = SSL_new (sslContext);
     if (sslHandle == NULL)
         errorHandler("ERROR SSL handle");
-    
     // Connect the SSL struct to our connection
     if(!SSL_set_fd(sslHandle, sfd))
         errorHandler("ERROR SSL set");
-    
      // Initiate SSL handshake
     if(SSL_connect(sslHandle) != 1)
         errorHandler("ERROR SSL connecting");
-        
     char buffer[BUFFER_LENGTH]={};
     char flagBuffer[BUFFER_LENGTH]={};
-    
     int result=0;
-    
     char solution [SOLUTION_LENGTH] ={};
     
     SSL_write (sslHandle, info, strlen (info));
@@ -194,18 +157,16 @@ void SSLConnection(int sfd, char *info)
     while (strstr(buffer,"BYE")==0)
     {
         result = calculator(buffer);
-        
+
         sprintf(solution, "%s %d\n","cs5700spring2014",result);
         memset(buffer, '\0', BUFFER_LENGTH);
         SSL_write (sslHandle, solution, strlen (solution));
         SSL_read (sslHandle, buffer, BUFFER_LENGTH);
     }
-    
     memcpy(flagBuffer, buffer+sizeof("cs5700spring2014"), FLAG_LENGTH);
 
     printf("Your secret flag is: \n%s\n",flagBuffer);
-    
-    
+
     if(sslHandle) {
         SSL_shutdown(sslHandle);
         SSL_free(sslHandle);
@@ -223,35 +184,30 @@ int main(int parameterCount, char *parameters[])
     struct hostent *h;    
     char info[MESSAGE_LENGTH] =  INITIAL_MESSAGE;
     char theTerminator[] = "\n";
+// Naming convetion in c:
 // All macros and constants in caps: MAX_BUFFER_SIZE, TRACKING_ID_"cs5700spring2014".
 // Struct names and typedef's in camelcase: GtkWidget, TrackingOrder.
 // Functions that operate on structs: classic C style: gtk_widget_show(), tracking_order_process().
 // Pointers: nothing fancy here: GtkWidget *foo, TrackingOrder *bar.
 // Global variables: just don't use global variables. They are evil.
 // Functions that are there, but shouldn't be called directly, or have obscure uses, or whatever: one or more underscores at the beginning: _refrobnicate_data_tables(), _destroy_cache().  
-  
-//     Parameter: 1  +   (host, nu_id) or (-p, port_number, host, nu_id), or ( -s, host, nu_id) or (-p, port_number, -s, host, nu_id)
+
+// Types of parameter:    
+//  Parameter: 1  +   (host, nu_id) or (-p, port_number, host, nu_id), or ( -s, host, nu_id) or (-p, port_number, -s, host, nu_id)
 // 1 + 2 or 4 or 3 or 5
-
-
     if(parameterCount == 6) {
-        
         // use strtol instead of atoi
         port = strtol(parameters[2],NULL,10);
        // printf("port is: %i\n", port);
         ssl_flag = 1;
         strcat(parameters[5], theTerminator);
         strcat(info, parameters[5]);
-        
     }else if(parameterCount == 5) {
-        
         port = strtol(parameters[2],NULL,10);
         strcat(parameters[4], theTerminator);
         strcat(info, parameters[4]);
         //printf("yes it's 5\n");
-    
     }else if(parameterCount == 4) {
-        
         port = DEFAULT_SSL_PORT;
         strcat(parameters[3], theTerminator);
         strcat(info, parameters[3]);
@@ -262,25 +218,21 @@ int main(int parameterCount, char *parameters[])
         strcat(parameters[2], theTerminator);
         //printf("parameters1 after strcat: %s\n",parameters[1] );
         //printf("checker.\n");
-
         strcat(info, parameters[2]);
         //printf("info at last: %s\n",info );
-        
     }else {
         //printf("parameters wrong.\n");
-        
         errorHandler("Error: Wrong parameters. Use format: <-p port> <-s> [hostname] [NEU ID]");
     }
 
-    int i;
-    for(i=0;i<parameterCount;i++){
-        printf("%s\n",parameters[i]);
-    }
+    // int i;
+    // for(i=0;i<parameterCount;i++){
+    //     printf("%s\n",parameters[i]);
+    // }
     // printf("info: %s\n",info);
     // printf("parameters1: %s\n",parameters[1]);
     // printf("parameters4: %s\n",parameters[4]);
     // printf("test end\n");
-
     
     socketFileDescriptor = socket(AF_INET, SOCK_STREAM, 0);
     if (socketFileDescriptor < 0)
@@ -308,7 +260,6 @@ int main(int parameterCount, char *parameters[])
     bcopy((char *)h->h_addr,
         (char *)&server_addr.sin_addr.s_addr,
           h->h_length);
-
     //printf("socketFileDescriptor: %i\n",socketFileDescriptor);
     //int size = sizeof(server_addr);
     //printf("sizeof(server_addr): %i\n",size);
@@ -321,8 +272,8 @@ int main(int parameterCount, char *parameters[])
         SSLConnection(socketFileDescriptor, info);
     }
     else{
-        printf("non ssl process.\n");
-        printf("info: %s\n", info);
+        // printf("non ssl process.\n");
+        // printf("info: %s\n", info);
         normalConnection(socketFileDescriptor, info);
     }
 
@@ -330,4 +281,3 @@ int main(int parameterCount, char *parameters[])
 
     return 0;
 }
-

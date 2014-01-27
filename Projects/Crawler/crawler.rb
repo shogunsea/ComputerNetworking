72e17c574b00d8875ca79fc76f8121f14d7a4546
shogunsea <shogunsea@foxmail.com>
2014-01-27 11:39:15 -0500

    complete login functionality, ready to crawl

--- Projects/Crawler/crawler.rb
+++ Projects/Crawler/crawler.rb
@@ -1,177 +1,133 @@
 require 'socket'
-require 'uri'
-# require 'debugger'
 class Crawler
-  attr_accessor :port
-  attr_accessor :host
-  attr_accessor :path
-  attr_accessor :socket
-  attr_accessor :query
-  attr_accessor :login_query
-  attr_accessor :cookie
-  attr_accessor :login_session
-  
-  # @host = 'cs5700.ccs.neu.edu'     # The web server
-  # @port = 80   
-  # @path = '/'
-  # @socket 
-  #the login query
-  @@post_query = "POST /accounts/login/?next=/fakebook HTTP/1.0\nHost: cs5700.ccs.neu.edu\nFrom: shogunx@ccs.neu.edu\nReferer: http://cs5700.ccs.neu.edu/accounts/login/?next=/fakebook/\nUser-Agent: HTTPTool/1.0\nContent-Type: application/x-www-form-urlencoded\nContent-Length: 56\r\n"
-  
-  
- # @@post_query= "POST /accounts/login/ HTTP/1.1
-# Host: cs5700.ccs.neu.edu
-# Connection: keep-alive
-# Content-Length: 109
-# User-Agent: HTTPTool/1.0
-# Content-Type: application/x-www-form-urlencoded
-# Referer: http://cs5700.ccs.neu.edu/accounts/login/?next=/fakebook/
-# 
-# username=001104765&password=ETLHFR62"
-  
-  #csrfmiddlewaretoken
-  
+  attr_accessor :port,:host,:path,:socket,:query,:login_query,:cookie,:login_session, :login_query
   
   def initialize
     @host = "cs5700.ccs.neu.edu"     # The web server
-    # @host = "google.com"     # The web server
     @port = 80   
     @path = "/"
     @socket = TCPSocket.open(host,port)
-    # @socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_KEEPALIVE, true)
     @login_query = "GET /accounts/login/?next=/fakebook HTTP/1.0\nFrom: shogunx@ccs.neu.edu\nConnection:keep-alive\nUser-Agent: HTTPTool/1.0\r\n\r\n"
     @query = "GET /fakebook HTTP/1.0\nFrom: shogunx@ccs.neu.edu\nConnection: keep-alive\nUser-Agent: HTTPTool/1.0\r\n\r\n"
   end
 
   def path=(new_path)
     @path = new_path
-    @query = "GET #{path} HTTP/1.0\nFrom: someuser@jmarshall.com\nUser-Agent: HTTPTool/1.0\r\n\r\n"
-    nil
+    @query.gsub!(/\/fakebook/, new_path)
   end
   
-  def update_query(cookie)
-    @query = "GET /fakebook HTTP/1.0\nFrom: shogunx@ccs.neu.edu\nCookie: csrftoken=#{cookie[:csrf]}; sessionid=#{self.login_session}\nConnection: keep-alive\nUser-Agent: HTTPTool/1.0\r\n\r\n"
+  def update_session(cookie)
+    @query = "GET /fakebook/ HTTP/1.0\nFrom: shogunx@ccs.neu.edu\nCookie: csrftoken=#{cookie[:csrf]}; sessionid=#{login_session}\nReferer: http://cs5700.ccs.neu.edu/accounts/login/?next=/fakebook/\nConnection: keep-alive\nUser-Agent: HTTPTool/1.0\r\n\r\n"
   end
   
   def get_cookie
-    response = self.get self.login_query
-    # debugger
+    response = get login_query
     csrf = response.scan(/csrftoken=\w*/).first.split("=")[1]
     session_id = response.scan(/sessionid=\w+/).first.split("=")[1]
-    
     @cookie={:csrf=>csrf,:session_id=>session_id}
   end
   
   def flush
-    @socket = TCPSocket.open(self.host,self.port) 
+    @socket = TCPSocket.open(host,port) 
   end
   
-  #method that handle login and session cookie.
+  #login method, through post request, will return new sessionid
   def login(username=nil,password=nil)
     cookie = get_cookie
     if username&&password
-      input="username=#{username}&password=#{password}\r\n\r\n"
+      input="csrfmiddlewaretoken=#{cookie[:csrf]}&username=#{username}&password=#{password}&next=%2Ffakebook%2F"
     else
-      input="csrfmiddlewaretoken=#{cookie[:csrf]}&username=001104765&password=ETLHFR62&next=%2Ffakebook%2F"
+      input="csrfmiddlewaretoken=#{cookie[:csrf]}&username=001104765&password=50UK7HD7&next=%2Ffakebook%2F"
     end
-    self.get_post_query input.length,cookie[:csrf],cookie[:session_id]
-    
-    login_query = Crawler.post_query+input
-    #call post here
-    p 'post request looks like:'
-    puts login_query
-    response = self.get(login_query)
-    if response
-      @login_session = response.scan(/sessionid=\w+/).first.split("=")[1] 
-      return @login_session
-    else
-      p 'response is nil, dont know why'
-    end
-    
-  end
-  
+    post_query = get_post_query(input.length,cookie[:csrf],cookie[:session_id])
+    login_query = post_query+input
+    response = get(login_query)
+    @login_session = response.scan(/sessionid=\w+/).first.split("=")[1] 
+  end   
   
   def crawl
     #success, can login and get 302 found response so far, and can get the session id after logedin.
-    self.login
-    #use sever returned session id to update the get query, no problem.
-    self.update_query(self.cookie)
+    new_session_id = login 
+    #get index page with new session id
+    update_session(cookie)
     
-    #try to get the index page, get 301 error right now.
-    self.get self.query
+    secreat_flags = bfs(query)
     
-    
-    
-    
+    puts secreat_flags
     
   end
   
-  #post method
-  def post
+  
+  def bfs(query)
+    queue = Queue.new
+    track = {}
+    track[url] = 1#dummy value
+    queue<<query # root node
+    secreat_flags = []
+    
+    while !queue.empty?
+      current_url = queue.pop
+      path = current_url
+      
+      current_response = get(query)
+      
+      adjcent_urls = parse_html(current_response)
+      
+      unless adjcent_urls.nil?
+        adjacent_urls.each do |url|
+          if track[url].nil?
+            track[url] = 1
+            queue<<url
+            
+            
+          end
+          
+        
+        end
+        
+      end
+      
+      
+      
+      
+      
+      
+    end
+    
+    
+    
+    
+    
+    
+    
+    return []
   end
+  
+  
   
   #get method
   def get(request=nil)
-    self.flush
+    flush
     @socket.write request
-    response= @socket.read#read method take soooooo long.
+    response= @socket.recv(10000000)#read method take soooooo long.
     #split("\r\n")[0]  
   end 
   
   def get_post_query(length,csrf,sessionid)
-    @@post_query = "POST /accounts/login/?next=/fakebook HTTP/1.0\nHost: cs5700.ccs.neu.edu\nFrom: shogunx@ccs.neu.edu\nReferer: http://cs5700.ccs.neu.edu/accounts/login/?next=/fakebook/\nUser-Agent: HTTPTool/1.0\nContent-Type: application/x-www-form-urlencoded\nContent-Length: #{length}\nCookie: csrftoken=#{csrf}; sessionid=#{sessionid}\r\n\r\n"
-    # self.query = "GET #{path} HTTP/1.0\nFrom: someuser@jmarshall.com\nUser-Agent: HTTPTool/1.0\n"
+    @login_query = "POST /accounts/login/?next=/fakebook HTTP/1.0\nHost: cs5700.ccs.neu.edu\nFrom: shogunx@ccs.neu.edu\nReferer: http://cs5700.ccs.neu.edu/accounts/login/?next=/fakebook/\nUser-Agent: HTTPTool/1.0\nContent-Type: application/x-www-form-urlencoded\nContent-Length: #{length}\nCookie: csrftoken=#{csrf}; sessionid=#{sessionid}\r\n\r\n"
+    # query = "GET #{path} HTTP/1.0\nFrom: someuser@jmarshall.com\nUser-Agent: HTTPTool/1.0\n"
   end
   
-  def self.post_query
-    @@post_query
+  
+  #BFS crawler. return secret flag found.
+  # def 
+  
+  
+  #method to parse html content, will return all valid url under 'cs5700.ccs.neu.edu' domain
+  def parse_html(html)
+    urls = html.scan(/\/fakebook\/\d+/)
   end
-
-  # def test
-    # @socket.print("GET / HTTP/1.0\nFrom: someuser@jmarshall.com\nUser-Agent: HTTPTool/1.0\r\n\r\n")
-    # @socket.read
-  # end
-#   
-    
   
 end
 
 
-
-# require 'net/http'
-# #require 'debugger'
-# 
-# ARGV.each do |a|
-  # p a
-# end
-# 
-# 
-# 
-# uri = URI('http://example.com/index.html')
-# # debugger
-# params = { :limit => 10, :page => 3 }
-# uri.query = URI.encode_www_form(params)
-# # debugger
-# 
-# res = Net::HTTP.get_response(uri)
-# # debugger
-# puts res.body if res.is_a?(Net::HTTPSuccess)
-# 
-# 
-# 
-# require 'socket'
-# 
-# host = 'www.google.com'
-# port = 80
-# 
-# s = TCPSocket.open host, port
-# s.puts "GET / HTTP/1.1\r\n"
-# s.puts "\r\n"
-# 
-# # while line = s.gets
-  # # puts line.chop
-# # end
-# a = s.read
-# p a
-# 
-# s.close
--- Projects/Crawler/fakebook.rb
+++ Projects/Crawler/fakebook.rb
@@ -4,6 +4,7 @@
 # 
 # username: Your NEU ID (with leading zeroes)
 # password: ETLHFR62
+# new password: 50UK7HD7
 # 
 # Good luck on the project!
 # 
@@ -12,8 +13,55 @@ require 'socket'
 require 'debugger'
 require 'uri'
 require './crawler'
+# include Socket::Constants
+# 
+# socket = Socket.new(AF_INET, SOCK_STREAM, 0)
+# sockaddr = Socket.sockaddr_in(80, 'www.google.com')
+# begin # emulate blocking connect
+  # socket.connect_nonblock(sockaddr)
+# rescue IO::WaitWritable
+  # IO.select(nil, [socket]) # wait 3-way handshake completion
+  # begin
+    # socket.connect_nonblock(sockaddr) # check connection failure
+  # rescue Errno::EISCONN
+  # end
+# end
+# debugger
+# socket.write("GET / HTTP/1.0\r\n\r\n")
+# results = socket.read
+# debugger
+
+
+tt = "HTTP/1.1 200 OK\r\nDate: Mon, 27 Jan 2014 15:36:41 GMT\r\nServer: Apache/2.2.22 (Ubuntu)\r\nVary: Accept-Language,Co
+okie,Accept-Encoding\r\nContent-Language: en-us\r\nSet-Cookie: sessionid=a5084ba028012ac96d7f35b311fa74cd; expires=Mo
+n, 10-Feb-2014 15:36:41 GMT; Max-Age=1209600; Path=/\r\nContent-Length: 1192\r\nKeep-Alive: timeout=5, max=100\r\nCon
+nection: Keep-Alive\r\nContent-Type: text/html; charset=utf-8\r\n\r\n<html><head><title>Fakebook</title><style TYPE=\"text/css\"><!--\n#pagelist li { display: inline; padding-right: 10px; }\n--></style></head><body><h1>Fakebook</h1><p
+><a href=\"/fakebook/\">Home</a></p><hr/><h1>Welcome to Fakebook</h1><p>Get started by browsing some random people's
+profiles!</p><ul><li><a href=\"/fakebook/636281627/\">Gol Koll</a></li><li><a href=\"/fakebook/636472562/\">Quinton W
+itaszek</a></li><li><a href=\"/fakebook/636531421/\">Patricia Alicer</a></li><li><a href=\"/fakebook/637560261/\">Del
+oris Lamik</a></li><li><a href=\"/fakebook/638078584/\">Pazu Nadutt</a></li><li><a href=\"/fakebook/638188865/\">Jero
+my Kilogan</a></li><li><a href=\"/fakebook/638480522/\">Zezerofibo Mishveladze</a></li><li><a href=\"/fakebook/639051
+328/\">Daxi Fesid</a></li><li><a href=\"/fakebook/639491321/\">Xofelehi Mccouch</a></li><li><a href=\"/fakebook/63971
+9376/\">Rat Starel</a></li></ul><h6>Fakebook is run by <a href=\"http://www.ccs.neu.edu/home/cbw/\">Christo Wilson</a
+> at                        \n<a href=\"http://www.northeastern.edu\">NEU</a>. It is meant for educational purposes o
+nly.                       \nFor questions, contact <a href=\"mailto:cbw@ccs.neu.edu\">Christo Wilson</a></h6></body>
+</html>\n"
+
+
+
  # shogunsea/foxmail/aaaaaa
  a = Crawler.new
+ debugger
+ socket = Socket.new(:INET, :STREAM,0)
+ sockaddr = Socket.pack_sockaddr_in( 80, 'cs5700.ccs.neu.edu' )
+ socket.connect( sockaddr )
+ debugger
+ socket.write( "GET / HTTP/1.0\r\n\r\n")
+ results = socket.read
+ # sock.connect(('agentx.ccs.neu.edu', 27993))  
+# sock.send('cs5700spring2013 HELLO 001105928\n')  
+# print sock.recv(1024)  
+ 
  debugger
  p 's'
  

fa96555b675b11608a26f71b3ae310f78d6b2239
shogunsea <shogunsea@foxmail.com>
2014-03-04 19:41:30 -0500

    add command line script

---
+++ Projects/RawSocket/RawSocket.rb
@@ -0,0 +1,9 @@
+
+
+class RawSocket
+	def initialize(url)
+		p 'RawSocket rocks!'
+		p "Ready to get target: #{url}..."
+	end
+
+end
---
+++ Projects/RawSocket/raw.rb
@@ -0,0 +1,10 @@
+require 'socket'
+# require 'debugger'
+require './RawSocket'
+
+
+ url = ARGV[0]
+
+ raw = RawSocket.new url
+
+
